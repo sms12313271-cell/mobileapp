278f32f2a15f65b3d17549fcf4aa17bf89dda094
sms12313271@gmail.com <sms12313271@gmail.com>
2025-09-26 22:11:57 +0900

    4주차 실습 2차 수정

--- w04/src/main/java/com/example/w04/MainActivity.kt
+++ w04/src/main/java/com/example/w04/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.w04
 
+import com.example.w04.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.w04.ui.theme.MyApplicationTheme
+import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,19 +35,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-
-
+                HomeScreen()
             }
         }
     }
 }
 
-// 수정 테스트
+// 프로필사진 지정
+@Composable()
+fun HomeScreen() {
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.dog),
+            contentDescription = "프로필 사진",
+            modifier = Modifier
+                .size(100.dp) // 이미지 크기 지정
+                .clip(CircleShape)
+        )
+        Text(
+            "손민석",
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            "강아지 사진",
+            color = MaterialTheme.colorScheme.onBackground,
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreenPreview() {
+    HomeScreen()
 }
